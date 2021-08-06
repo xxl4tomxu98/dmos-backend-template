90a9ec8c186071e816319afacdf184b218f00c65
Griff Tatarsky <griffith.tatarsky@associates.fema.dhs.gov>
2021-08-06 11:44:54 -0400

    DMOS-30 | Functional Swagger

--- src/main/java/com/backend/template/dmosbackendtemplate/DmosBackendTemplateApplication.java
+++ src/main/java/com/backend/template/dmosbackendtemplate/DmosBackendTemplateApplication.java
@@ -2,12 +2,12 @@ package com.backend.template.dmosbackendtemplate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
 public class DmosBackendTemplateApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DmosBackendTemplateApplication.class, args);
     }
-
 }
