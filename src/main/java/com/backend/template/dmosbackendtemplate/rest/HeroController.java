cb3898ebbba0560541ce9e0c8279fb3b1bfdf6fd
rab-ftc <84587096+rab-ftc@users.noreply.github.com>
2021-08-11 23:20:39 -0400

    Feature/DMOS-52 | POC SageMaker Invocation (#13)
    * DMOS-52 feat: sentiment endpoint
    
    * fix: turn off h2 console
    
    * fix: change liquibase contest
    
    * revert change
    
    * hit endpoint
    
    * fix: change content type to application/json
    
    * try csv
    
    * try simple review
    
    Co-authored-by: Robert Bergstrom <rbergstrom@ftc-llc.om>

--- src/main/java/com/backend/template/dmosbackendtemplate/rest/HeroController.java
+++ src/main/java/com/backend/template/dmosbackendtemplate/rest/HeroController.java
@@ -18,47 +18,79 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.core.SdkBytes;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sagemakerruntime.SageMakerRuntimeClient;
+import software.amazon.awssdk.services.sagemakerruntime.model.InvokeEndpointRequest;
+import software.amazon.awssdk.services.sagemakerruntime.model.InvokeEndpointResponse;
+
 @RestController
 @RequestMapping("/api/heroes")
-public class HeroController
-{
+public class HeroController {
 
     @Inject
     private HeroService heroService;
 
     @GetMapping
-    @RolesAllowed({"dmos_read"})
+    @RolesAllowed({ "dmos_read" })
     public List<HeroDTO> getAllHeroes() {
         return heroService.getAllHeroes();
     }
 
     @GetMapping("/{heroId}")
     @RolesAllowed("dmos_read")
-    public HeroDTO getHero(@PathVariable("heroId") Integer heroId)
-    {
+    public HeroDTO getHero(@PathVariable("heroId") Integer heroId) {
         return heroService.getHero(heroId, null);
+    }
+
+    @GetMapping("/sentiment")
+    @RolesAllowed("dmos_read")
+    public String getSentiment() {
+        // Set Variables
+        String endpointName = "pytorch-inference-2021-08-10-19-25-44-438";
+        String contentType = "text/csv";
+        String awsCredentialsProfileName = "default";
+
+        // Read payload into a variable
+        SdkBytes body = SdkBytes.fromUtf8String("The simplest pleasures in life are the best, and this film is one of them. Combining a rather basic storyline of love and adventure this movie transcends the usual weekend fair with wit and unmitigated charm.");
+
+        // Build an Invocation request object
+        InvokeEndpointRequest request = InvokeEndpointRequest.builder().contentType(contentType).body(body)
+                .endpointName(endpointName).build();
+
+        // Load credentails into a profile
+        ProfileCredentialsProvider profile = ProfileCredentialsProvider.builder().profileName(awsCredentialsProfileName)
+                .build();
+
+        // Build AmazonSageMakerRuntime client
+        SageMakerRuntimeClient runtime = SageMakerRuntimeClient.builder().credentialsProvider(profile)
+                .region(Region.US_EAST_1).build();
+
+        // Perform an inference
+        InvokeEndpointResponse result = runtime.invokeEndpoint(request);
+
+        // Print inference result
+       return result.body().asUtf8String();
     }
 
     @PostMapping
     @ApiOperation("Create Hero")
-//    @RolesAllowed("dmos_write")
-    public HeroDTO createHero(
-            @Valid @NotNull @RequestBody HeroDTO dto) {
+    // @RolesAllowed("dmos_write")
+    public HeroDTO createHero(@Valid @NotNull @RequestBody HeroDTO dto) {
         return new HeroDTO(heroService.createHero(dto));
     }
 
     @PutMapping("/{id}")
     @ApiOperation("Update Hero")
-    public HeroDTO updateHero(
-            @Valid @NotNull  @PathVariable(value = "id") Integer id,
+    public HeroDTO updateHero(@Valid @NotNull @PathVariable(value = "id") Integer id,
             @Valid @NotNull @RequestBody HeroDTO heroDTO) {
         return new HeroDTO(heroService.updateHero(id, heroDTO));
     }
 
     @DeleteMapping("/{id}")
     @ApiOperation("Delete Hero")
-    public void deleteComment(
-            @Valid @NotNull  @PathVariable(value = "id") Integer id) {
+    public void deleteComment(@Valid @NotNull @PathVariable(value = "id") Integer id) {
         heroService.deleteHero(id);
     }
 }
