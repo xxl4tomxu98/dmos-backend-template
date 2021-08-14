93bdb3a76f3264f2da88ad5c8ece45c6a8295f1e
Robert Bergstrom <rbergstrom@ftc-llc.om>
2021-08-14 14:11:02 +0000

    fix: removed cluster setup and with db password

--- cluster/initdb.sql
+++
@@ -1,6 +0,0 @@
-create database dmos_dev;
-create database dmos_test;
-create database dmos_prod;
-
---- Create schemas (First connect to correct database)
-create schema dmos_main authorization dmos_backend;
